99b9d870fca21aa0daea2927ba6f75b3ea849a86
Addison Severnak <addisonsevernak@gmail.com>
2023-01-29 13:24:18 -0600

    added initial todos object and add todo function

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { ReactDOM } from 'react';
+
+const todos = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    }
+  };
+
   render() {
     return (
       <div>
-        Todo App
+        <div>
+          <h1>Todo App</h1>
+        </div>
+        <button>Hide Completed</button>
       </div>
     )
   }
